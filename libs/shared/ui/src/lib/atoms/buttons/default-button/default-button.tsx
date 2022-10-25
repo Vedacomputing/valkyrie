941b39b41d230f7aac50ac8fc416ff6e0f38e152
Gustavo Cabezal <meisterveda@gmail.com>
2022-10-25 01:38:01 +0000

    feat: Updated Default Hero
    Added tailwind to storyboobks and all apps
    Added Default Hero

--- libs/shared/ui/src/lib/atoms/buttons/default-button/default-button.tsx
+++ libs/shared/ui/src/lib/atoms/buttons/default-button/default-button.tsx
@@ -5,9 +5,7 @@ export interface DefaultButtonProps {}
 
 export function DefaultButton(props: DefaultButtonProps) {
   return (
-    <div className={styles['container']}>
-      <h1>Welcome to DefaultButton!</h1>
-    </div>
+    <button type="button" className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800">Default</button>
   );
 }
 
